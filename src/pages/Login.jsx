9b2da18fda2a2162ee43465c3b5b77cd1d770591
Akashdhage <dhageakash2004@gmail.com>
2025-08-20 11:20:39 +0530

    add login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import {ToastContainer,toast} from 'react-toastify';
+
 
 export default function Login() {
+
+    const [values,setValues] = useState({
+        email:"",
+        password:""
+    });
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform registration logic here
+    // toast.success("Registration successful!");
+};
+
   return (
-    <div>Login</div>
+    <div className='container'>
+        <h2>Login Account</h2>
+        <form onSubmit={(e) =>handleSubmit(e)}>
+            {/* <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder='Email' />
+            </div> */}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder='Email'
+                 onChange={(e) => setValues({...values,[e.target.name]:e.target.value})} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder='Password'
+                 onChange={(e) => setValues({...values,[e.target.name]:e.target.value})} />
+
+            </div>
+            <button type="submit">Submit</button>
+            <span>
+                Already have an account? <Link to="/Register">Register</Link>
+            </span>
+        </form>
+        <ToastContainer/>
+    </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import {ToastContainer,toast} from 'react-toastify';
+
 
 export default function Register() {
+
+    const [values,setValues] = useState({
+        email:"",
+        password:""
+    });
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform registration logic here
+    // toast.success("Registration successful!");
+};
+
   return (
-    <div>Register</div>
+    <div className='container'>
+        <h2>Register Account</h2>
+        <form onSubmit={(e) =>handleSubmit(e)}>
+            {/* <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder='Email' />
+            </div> */}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder='Email'
+                 onChange={(e) => setValues({...values,[e.target.name]:e.target.value})} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder='Password'
+                 onChange={(e) => setValues({...values,[e.target.name]:e.target.value})} />
+
+            </div>
+            <button type="submit">Submit</button>
+            <span>
+                Already have an account? <Link to="/login">Login</Link>
+            </span>
+        </form>
+        <ToastContainer/>
+    </div>
   )
 }
